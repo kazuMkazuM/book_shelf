eaa2be043c405888ba9e3863f79357fe57dabc43
kazuMkazuM <m_m_kzym_m_kzy@yahoo.co.jp>
2019-05-03 17:37:39 +0900

    bookのNewとCreate完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,3 +3,16 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 end
+
+def new
+  @book = Book.new
+end
+def create
+  @book = Book.new(book_params)
+  @book.save
+  redirect_to @book, notice: "書籍を登録しました。"
+end
+private
+def book_params
+  params.require(:book).permit(:title, :price, :publish_date, :description)
+end
